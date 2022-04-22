defde0d38b40614ccf48f02adf953c0973b62b4c
Kevin Gardner <whelm81@yahoo.com>
2022-04-21 21:41:26 -0700

    Add Flask html documents and database layer

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from __future__ import print_function
+from flask import Flask, url_for, flash, request, session, redirect, render_template, jsonify, make_response
+from datetime import datetime
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return redirect(url_for('load_home'))
+    # ''' This will route the user to the home page.'''
+    # if 'username' in session:
+    #     return redirect(url_for('load_home'))
+    # else:
+    #     return render_template('user-login-form.html', title='Login Page')
 
+@app.route('/home/')
+def load_home():
+        return render_template('index.html', title='Library Management System')
 
 if __name__ == '__main__':
     app.run()
